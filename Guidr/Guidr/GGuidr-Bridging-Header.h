b49125b11a965b2d06ae474892cc820030e5e001
kizerxl <changoof@gmail.com>
2016-05-25 17:32:30 -0400

    first attempt

---
+++ Guidr/Guidr/GGuidr-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  Pods
+//
+//  Created by Felix Changoo on 5/25/16.
+//
+//
+
+#ifndef Header_h
+#define Header_h
+
+ #import <GTMOAuth2/GTMOAuth2Authentication.h>
+
+
+#endif /* Header_h */
